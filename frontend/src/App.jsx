a08951fb86a487de5eb02e15eea9a2f9e96f3495
Tanuj Kumar <tanujmaurya40@gmail.com>
2024-09-15 16:04:52 +0530

    2nd part Complete- Design App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+
 import './App.css';
+import Home from './pages/Home/Home';
+
 
 function App() {
 
   return (
-    <>
-
-
-    </>
+    <div className='p-4 h-screen flex justify-center items-center'>
+      <Home />
+    </div>
   )
 }
 
