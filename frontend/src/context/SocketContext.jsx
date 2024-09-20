4acec919b1975ddf63bc9df932e114ca843d7326
Tanuj Kumar <tanujmaurya40@gmail.com>
2024-09-20 11:28:32 +0530

    SocketContext Url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io(`http://localhost:8000`, {
+            const socket = io(`https://chat-app-prod-04jl.onrender.com`, {
                 query: {
                     userId: authUser._id
                 }
